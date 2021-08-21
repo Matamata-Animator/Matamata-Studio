18c10f2d0ef92c2ab7e43f897cd840518225ddf4
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-08-20 23:19:06 -0400

    added extras card template

---
+++ src/extras/extras.ts
@@ -0,0 +1,10 @@
+// <div class="w3-card-4 card" style="background-color: var(--mm-selection);"
+// onclick="window.open('https://www.w3schools.com')">
+// <img src="thumbnails/photopea.png">
+// <div class="w3-container w3-center">
+//   <p><strong>Title</strong></p>
+//   <p>Description</p>
+// </div>
+// </div>
+
+import * as fs from "fs";

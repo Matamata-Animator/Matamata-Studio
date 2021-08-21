e171e6209c3d8d9553f18d498f4c55e92286c220
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-08-20 23:50:49 -0400

    loads extras crds from json file

--- src/extras/extras.ts
+++ src/extras/extras.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from "fs";
 // <div class="w3-card-4 card" style="background-color: var(--mm-selection);"
 // onclick="window.open('https://www.w3schools.com')">
 // <img src="thumbnails/photopea.png">
@@ -6,5 +7,34 @@
 //   <p>Description</p>
 // </div>
 // </div>
+let raw = readFileSync("build/extras/extras.json");
+let json = JSON.parse(raw.toString());
+let extras = json.extras;
 
-import * as fs from "fs";
+console.log(extras);
+
+let deck = document.getElementById("cardDeck");
+
+for (const e of extras) {
+  console.log(e);
+
+  let card = document.createElement("div");
+  card.className = "w3-card-4 card";
+  card.onclick = () => window.open(`${e.url}`);
+
+  let image = document.createElement("img");
+  image.src = e.thumbnail;
+
+  let text = document.createElement("div");
+  text.className = "w3-container w3-center";
+  text.innerHTML = ` 
+     <p><strong>${e.title}</strong></p>
+   <p>${e.description}</p> 
+  `;
+
+  card.appendChild(image);
+  card.appendChild(text);
+  deck?.append(card);
+}
+
+// console.log(JSON.parse(raw.toString()));
